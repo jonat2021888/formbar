a3fdd1ecf91e07fc775febdbd44d262cdd6ae9ab
jonat2021888 <56306763+jonat2021888@users.noreply.github.com>
2021-03-04 11:16:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -146,6 +146,19 @@ def clearBar():
     for pix in range(0, BARPIX):
         pixels[pix] = colors['default']
 
+def countdown(t):
+ while t:
+        mins, secs = divmod(t, 4)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+# input time in seconds
+t = input("Enter the time in seconds: 4")
+# function call
+countdown(int(t))
+
+
 def clearString():
     for i in range(BARPIX, MAXPIX):
         pixels[i] = colors['bg']
